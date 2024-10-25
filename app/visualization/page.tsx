7bdf7905f94f68732272a4aef04ed6f8f63faa36
Nicolas Filippelli <nicolas.filippelli@coupa.com>
2024-10-25 10:17:01 +0200

    clean up

--- app/visualization/page.tsx
+++
@@ -1,8 +0,0 @@
-import { VisualizationWindow } from "@/components/VisualizationWindow";
-
-export default function VisualizationPage() {
-  return (
-    <VisualizationWindow endpoint={""} emoji="🏴‍☠️"
-                         titleText="Dynamic system data visualization" />
-  )
-}
